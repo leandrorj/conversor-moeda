727f501248b08f8fcdc4080a86300c54cfc758e3
leandrorj <leandro__brasil@hotmail.com>
2018-12-17 17:42:58 -0300

    acessando os valores do arquivo JSON

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main(){
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+
+const request = 'https://api.hgbrasil.com/finance?format=json&key=827016a7';
+
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+
   runApp(MaterialApp(
     home: Container(),
   ));
