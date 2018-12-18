86e7e8c28734c4535f2f6f2a6a49c5787b40334d
leandrorj <leandro__brasil@hotmail.com>
2018-12-18 09:55:19 -0300

    adicionando um scaffold com um AppBar e metodo de captura de dados JSON

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,34 @@ const request = 'https://api.hgbrasil.com/finance?format=json&key=827016a7';
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
